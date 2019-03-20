f8d5223f4fd191a0b6fcf56d10dcbd9967a8feb8
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-20 15:30:17 -0700

    Add UserTypeSelection activity with layout.  Update app colors to match wireframe.

---
+++ app/src/main/java/com/jbseppanen/birthride/UserTypeSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.jbseppanen.birthride
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserTypeSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_type_selection)
+    }
+}
