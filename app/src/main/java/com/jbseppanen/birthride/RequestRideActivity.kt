71deb3e63f94604c88e18b4463d7ace87d1bff0a
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-20 17:06:13 -0700

    Add RequestRide activity with layout.

---
+++ app/src/main/java/com/jbseppanen/birthride/RequestRideActivity.kt
@@ -0,0 +1,12 @@
+package com.jbseppanen.birthride
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RequestRideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request_ride)
+    }
+}
